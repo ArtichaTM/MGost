70b70d1690b3a8820cc6ef7831ac3e812fe75e30
Articha <tima-1324@mail.ru>
2025-07-20 00:24:46 +0300

    Added no command command

--- src/mgost/cli/app.py
+++ src/mgost/cli/app.py
@@ -1,12 +1,29 @@
-from typer import Typer
+from pathlib import Path
+from tomllib import load
 
+from typer import Typer
 
 __all__ = ('app', )
 
 
-app = Typer()
+app = Typer(name="MGost")
 
 
 @app.command()
-def xz():
-    print('Q')
+def no_command():
+    path_pyproject = (
+        Path(__file__)
+        .parent
+        .parent
+        .parent
+        .parent
+        .joinpath('pyproject.toml')
+    )
+    if not path_pyproject.exists():
+        print("MGost api app")
+    with path_pyproject.open('rb') as f:
+        pyproject = load(f)
+        project = pyproject['project']
+        name = project["name"]
+        version = project['version']
+    print(f"{name} version {version}")
